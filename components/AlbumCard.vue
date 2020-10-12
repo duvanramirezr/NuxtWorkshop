9b98d816635072256e49ca373055bfda1ff6dad0
duvanramirezr <duaramirezr@correo.udistrital.edu.co>
2020-10-12 13:47:32 -0500

    primer acercamiento a nuxt

---
+++ components/AlbumCard.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+    <div class=" column is-one-quarter">
+        <div class="box">
+<h3>{{album.title}}</h3>
+<nuxt-link :to="`/albums/${album.id}`" class="button is-primary"> ver álbum</nuxt-link>
+
+ </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'AlbumCard',
+        props:{
+            album: Object 
+        }
+    }
+</script>
+
+<style lang="css" >
+.box{
+    padding:1em;
+
+}
+h3{
+    min-height: 5em;
+}
+</style>
